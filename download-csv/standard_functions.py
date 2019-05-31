5a7ef096d6b75b2380ca70d65fe757110c2b07f2
Jack <48194176+jharmon96@users.noreply.github.com>
2019-05-31 13:58:49 -0400

    Update standard_functions.py

--- download-csv/standard_functions.py
+++ download-csv/standard_functions.py
@@ -1,10 +1,10 @@
-import csv
-import os
-from pynput.keyboard import Key
-import time
 import settings
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from pathlib import Path
 
 home = str(Path.home())
@@ -14,18 +14,23 @@ home = str(Path.home())
 
 def initialize_browser():
     chromeOptions = webdriver.ChromeOptions()
-    prefs = {"download.default_directory": r"C:\unanet_imu_CSV\Export Files"}
+    prefs = {"download.default_directory": settings.source_path + r"\unanet_AQC_imu\reference_files"}
     chromeOptions.add_experimental_option("prefs", prefs)
-    #chromeOptions.add_argument("--headless")
-    chromeDriver = r"C:\unanet_imu_CSV\drivers\chromedriver.exe"
-    settings.driver = webdriver.Chrome(executable_path=chromeDriver, chrome_options=chromeOptions)
+    # chromeOptions.add_argument("--headless")
+    caps = DesiredCapabilities().CHROME
+    # caps["pageLoadStrategy"] = "normal"  #  complete
+    caps["pageLoadStrategy"] = "none"  # interactive
+    chromeDriver = settings.source_path + r'\unanet_AQC_imu\python\driver\chromedriver.exe'
+    settings.driver = webdriver.Chrome(desired_capabilities=caps, executable_path=chromeDriver,
+                                       chrome_options=chromeOptions)
 
 
 # logs into the browser with credentials provided in settings.py
 
 def login():
-    settings.driver.get(settings.URL + "/home")
-
+    wait = WebDriverWait(settings.driver, 10)
+    settings.driver.get(settings.URL + "/login")
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#username')))
     id_box = settings.driver.find_element_by_name('username')
     id_box.send_keys(settings.username)
 
@@ -34,4 +39,3 @@ def login():
 
     login_button = settings.driver.find_element_by_name('button_ok')
     login_button.click()
-
